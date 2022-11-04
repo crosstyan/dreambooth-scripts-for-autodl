4341b471d70a7a222586e31507f6a4cb5113d092
crosstyan <crosstyan@outlook.com>
2022-11-04 14:37:27 +0800

    using Invoke-WebRequest

--- get_alt_script.ps1
+++ get_alt_script.ps1
@@ -1,3 +1,3 @@
 # download the alternative script from
 # https://colab.research.google.com/drive/17yM4mlPVOFdJE_81oWBz5mXH9cxvhmz8#scrollTo=aLWXPZqjsZVV
-wget -O repos/diffusers/examples/dreambooth/train_dreambooth_alt.py https://pub-2fdef7a2969f43289c42ac5ae3412fd4.r2.dev/dreambooth.py
+Invoke-WebRequest -OutFile repos/diffusers/examples/dreambooth/train_dreambooth_alt.py https://pub-2fdef7a2969f43289c42ac5ae3412fd4.r2.dev/dreambooth.py
